6271fc71b54011e5dba90fc21c38c74e24de06e1
KhairUnNisa10 <khairunnisa1519@yahoo.com>
2025-11-03 14:12:34 +0500

    Initial commit - Added Ranain Visuals project files.

---
+++ intro-js/index.js
@@ -0,0 +1,91 @@
+// Our Process Start
+(function ($) {
+  $(function () {
+    // Cache selectors
+    var $window = $(window),
+      $agTimeline = $(".js-timeline"),
+      $agTimelineLine = $(".timeline_line"),
+      $agTimelineLineProgress = $(".timeline_line_progress"),
+      $agTimelinePoint = $(".js-timeline-card_point-box"),
+      $agTimelineItem = $(".timeline_item"),
+      agOuterHeight = $window.outerHeight(),
+      agHeight = $window.height(),
+      lastScrollY = -1,
+      isUpdating = false;
+
+    // Attach event listeners
+    $window.on("scroll", onScroll);
+    $window.on("resize", onResize);
+
+    function onScroll() {
+      updateFrame();
+    }
+
+    function onResize() {
+      agHeight = $window.height();
+      updateFrame();
+    }
+
+    function updateWindow() {
+      isUpdating = false;
+
+      // Update the position of the timeline line
+      $agTimelineLine.css({
+        top:
+          $agTimelineItem.first().find($agTimelinePoint).offset().top -
+          $agTimelineItem.first().offset().top,
+        bottom:
+          $agTimeline.offset().top +
+          $agTimeline.outerHeight() -
+          $agTimelineItem.last().find($agTimelinePoint).offset().top,
+      });
+
+      if (lastScrollY !== $window.scrollTop()) {
+        lastScrollY = $window.scrollTop();
+        updateProgress();
+      }
+    }
+
+    function updateProgress() {
+      var lastItemTop = $agTimelineItem
+          .last()
+          .find($agTimelinePoint)
+          .offset().top,
+        currentScrollY = $window.scrollTop(),
+        progressTop =
+          $agTimelineLineProgress.offset().top +
+          currentScrollY -
+          currentScrollY,
+        middleOfWindow = currentScrollY + agOuterHeight / 2.5;
+
+      var progressHeight = Math.min(
+        middleOfWindow - progressTop,
+        lastItemTop - progressTop
+      );
+      $agTimelineLineProgress.css({
+        height: progressHeight + "px",
+      });
+
+      // Toggle active class based on scroll position
+      $agTimelineItem.each(function () {
+        var itemTop = $(this).find($agTimelinePoint).offset().top;
+        if (itemTop + currentScrollY - currentScrollY < middleOfWindow) {
+          $(this).addClass("js-ag-active");
+        } else {
+          $(this).removeClass("js-ag-active");
+        }
+      });
+    }
+
+    function updateFrame() {
+      if (!isUpdating) {
+        requestAnimationFrame(updateWindow);
+        isUpdating = true;
+      }
+    }
+
+    // Initial update
+    updateFrame();
+  });
+})(jQuery);
+// Our Process End
---
+++ intro-js/intro.js
@@ -0,0 +1,103 @@
+$(function() {
+    "use strict";
+    $(window).on("load", function() {
+        // Preloader
+        $("#preloader").fadeOut(600);
+        $(".preloader-bg").delay(400).fadeOut(600);
+        // fadeIn.element
+        setTimeout(function() {
+            $(".fadeIn-element").delay(600).css({
+                display: "none"
+            }).fadeIn(800);
+        }, 0);
+    });
+
+    // Page scroll
+    $(".page-scroll").on("click", function(e) {
+        var $anchor = $(this);
+        $("html, body").stop().animate({
+            scrollTop: $($anchor.attr("href")).offset().top - 55
+        }, 1500, 'easeInOutExpo');
+        e.preventDefault();
+    });
+    // Highlight Navigation
+    $("body").scrollspy({
+        target: ".navbar",
+        offset: 65
+    });
+    // Close Mobile Menu
+    $(".navbar-collapse ul li a").on("click", function() {
+        $(".navbar-toggle:visible").click();
+    });
+    // Highlight Navigation
+    $(".link-underline-menu").on("click", function() {
+        $(".link-underline-menu").removeClass("active");
+        $(this).addClass("active");
+    });
+    $(window).on("scroll", function() {
+        // Collapse Navigation
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-bg-switch").addClass("main-navigation-bg");
+        } else {
+            $(".navbar-bg-switch").removeClass("main-navigation-bg");
+        }
+
+        // To Top Arrow Animation
+        if ($(this).scrollTop() > 400) {
+            $(".to-top-arrow").addClass("show");
+        } else {
+            $(".to-top-arrow").removeClass("show");
+        }
+
+        // Home FadeOut Animation
+        $("h1.home-page-title, h2.home-page-title, .the-button, .the-button-light, .scroll-line, .more-wraper-center.more-wraper-center-home").css("opacity", 1 - $(window).scrollTop() / 500);
+    });
+    
+    // Slick Slider Benefits
+    $(".slick-benefits").slick({
+        arrows: true,
+        initialSlide: 0,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: "<i class='slick-prev icon ion-chevron-left'></i>",
+        nextArrow: "<i class='slick-next icon ion-chevron-right'></i>",
+        fade: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        cssEase: "ease",
+        speed: 600
+    });
+
+	// Clone Function
+    $.fn.duplicate = function(count, cloneEvents, callback) {
+        var stack = [],
+        el;
+        while (count--) {
+            el = this.clone(cloneEvents);
+            callback && callback.call(el);
+            stack.push(el.get()[0]);
+        }
+        return this.pushStack(stack);
+    };
+    $("<div class='vertical-lines-wrapper'></div>").appendTo(".vertical-lines");
+    $("<div class='vertical-effect'></div>").duplicate(3).appendTo(".vertical-lines-wrapper");
+});
+
+const disclaimer = document.querySelector('.disclaimer');
+const modal = document.getElementById('modal');
+const closeBtn = document.querySelector('.close');
+
+disclaimer.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+});
